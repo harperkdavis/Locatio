4c27f01ab52331b248ade9ad6c3e3a42f4e30741
Double Black <davish@catlin.edu>
2020-03-31 19:02:52 -0700

    Add basic name generation

--- Locatio.py
+++ Locatio.py
@@ -1,9 +1,17 @@
-import json
+import json, math, random
+from staticon import Level, sprint
+
+with open('names.json') as f:
+    names = json.load(f)
 
 def generate_name(loc):
-
-    pass
+    first = names[loc]['firsts'] + names['global']['firsts']
+    last = names[loc]['lasts'] + names['global']['lasts']
+    location_name = random.choice(first)
+    if random.random() < names['lastChance']:
+        location_name += random.choice(last)
+    return location_name + ' ' + loc
 
 
 if __name__ == '__main__':
-    pass
+    print(generate_name('ridge').title())
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #Locatio
+
 Nothing yet
